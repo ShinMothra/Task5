53fe6866cde7c342accf291453b5afcb8d28ffea
ShinMothra <145927979+ShinMothra@users.noreply.github.com>
2023-12-20 13:14:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var productPrice = parseFloat(document.getElementById('product').value);
     var quantity = parseInt(document.getElementById('quantity').value);
 
-    if (!isNaN(productPrice) && !isNaN(quantity)) {
+    if (!isNaN(productPrice) && !isNaN(quantity) && (quantity)>0) {
       var totalCost = productPrice * quantity;
       document.getElementById('totalCost').textContent = 'Итоговая стоимость: ₽' + totalCost;
     } else {
